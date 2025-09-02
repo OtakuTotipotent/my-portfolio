81373820ae33f6d8b4fb2482c6934dc325f2031b
Afnan Muhammad <otakutotipotent@gmail.com>
2025-09-02 14:29:07 +0500

    add project

---
+++ mywork/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Project
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "title",
+            "description",
+            "image",
+            "project_url",
+            "github_url",
+        ]
--- mywork/views.py
+++ mywork/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project
+from .forms import ProjectForm
 
 
 def index(request):
     projects = Project.objects.all()
     return render(request, "mywork/index.html", {"projects": projects})
+
+
+def add_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = ProjectForm()
+    context = {"form": form}
+    return render(request, "mywork/add_project.html", context)
