121f8d257078372ba1e17b4be00d55d10b9f99d2
Afnan Muhammad <otakutotipotent@gmail.com>
2025-09-02 11:06:46 +0500

    media settings & media models

--- mywork/models.py
+++ mywork/models.py
@@ -1,11 +1,13 @@
+import os
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="uploads/projects/", blank=True)
+    image = models.ImageField(upload_to="projects", blank=True)
     project_url = models.URLField(blank=True)
     github_url = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -19,3 +21,16 @@ class Project(models.Model):
 
     def get_absolute_url(self):
         return reverse("project-detail", kwargs={"pk": self.pk})
+
+    def save(self, *args, **kwargs):
+        if self.image and not self.pk:
+            file_extension = os.path.splitext(self.image.name)[1]
+            safe_filename = f"{slugify(self.title)}{file_extension}"
+            self.image.name = safe_filename
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs)
--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# serving media files for development
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("my-work/", include("mywork.urls")),
     path("page", include("pages.urls")),
 ]
+
+# serving media files for development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
